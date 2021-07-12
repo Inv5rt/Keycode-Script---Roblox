05db101a12e40421b393caad94dcc44992c63249
Invert <83333098+Inv5rt@users.noreply.github.com>
2021-07-12 14:10:31 +0300

    Add files via upload

---
+++ Keycode Script - Lua.lua
@@ -0,0 +1,5 @@
+game:GetService("UserInputService").InputBegan:Connect(function(inputObject, gameProcessedEvent)
+if inputObject.KeyCode == Enum.KeyCode.YOURKEYBIND then
+       -- Script Here
+    end
+end)
